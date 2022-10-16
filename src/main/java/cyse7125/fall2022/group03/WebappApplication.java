edae2ac1a8e0b424c41e26f34d3dcf0b83f211e0
Vignesh Gunasekaran <gunasekaran.v@northeastern.edu>
2022-10-16 12:12:38 -0400

    spring boot project

---
+++ src/main/java/cyse7125/fall2022/group03/WebappApplication.java
@@ -0,0 +1,13 @@
+package cyse7125.fall2022.group03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cyse7125/fall2022/group03/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package cyse7125.fall2022.group03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
